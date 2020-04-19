651eeb2ea70e3e28cbdddf875960b4e778f4a376
fanshan <devfanshan@gmail.com>
2020-04-19 17:36:26 +0800

    1.Google AdMob添加测试设备

--- model_main/src/main/java/com/future/main/ui/SplashActivity.kt
+++ model_main/src/main/java/com/future/main/ui/SplashActivity.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import com.fsh.common.base.BaseActivity
 import com.future.main.R
-import com.google.android.gms.ads.AdListener
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.InterstitialAd
-
+import com.google.android.gms.ads.*
+import java.util.*
 
 
 class SplashActivity : BaseActivity() {
@@ -22,9 +19,16 @@ class SplashActivity : BaseActivity() {
 //            startActivity(Intent(this@SplashActivity,MainActivity::class.java))
 //            finish()
 //        },3000)
-        MobileAds.initialize(this,"ca-app-pub-3940256099942544~3347511713")/*{
+        MobileAds.initialize(this){
             Log.d("SplashActivity","MobileAds initialize status $it ${it.adapterStatusMap}")
-        }*/
+        }
+        //RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("8819A7B8E6651637DA0C7174E8843077")
+        MobileAds.setRequestConfiguration(
+            RequestConfiguration.Builder()
+                .setTestDeviceIds(
+                listOf("8819A7B8E6651637DA0C7174E8843077")
+            ).build()
+        )
         val mInterstitialAd = InterstitialAd(this)
         mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
         mInterstitialAd.adListener = object : AdListener() {
